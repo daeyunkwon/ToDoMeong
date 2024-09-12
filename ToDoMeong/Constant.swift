fa93716e2cb2fb27cf451eac11cb1ec6804cff3c
Daeyun <sweet_ray@naver.com>
2024-09-12 18:57:08 +0900

    Chore: 외부 폰트 추가

---
+++ ToDoMeong/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  ToDoMeong
+//
+//  Created by 권대윤 on 9/12/24.
+//
+
+import SwiftUI
+
+enum Constant {
+    
+    enum AppFont {
+        static let jalnanBold23 = Font.custom("Jalnan2", size: 23).bold()
+    }
+    
+}
