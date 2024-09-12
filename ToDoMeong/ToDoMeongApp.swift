32bd5fef1f33be6d849cd1b135aef8c06dce9a91
Daeyun <sweet_ray@naver.com>
2024-09-12 17:55:16 +0900

    Initial Commit

---
+++ ToDoMeong/ToDoMeongApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoMeongApp.swift
+//  ToDoMeong
+//
+//  Created by 권대윤 on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoMeongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
