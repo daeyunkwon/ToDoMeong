4aa2cd89496fa6971ee64c3825584f86c51a04de
Daeyun <sweet_ray@naver.com>
2024-09-13 18:03:34 +0900

    Chore: 외부 폰트 리소스 추가

--- ToDoMeong/Constant.swift
+++ ToDoMeong/Constant.swift
@@ -10,7 +10,19 @@ import SwiftUI
 enum Constant {
     
     enum AppFont {
-        static let jalnanBold23 = Font.custom("Jalnan2", size: 23).bold()
+        static let jalnanTopLeading = Font.custom("Jalnan2", size: 23)
+        static let jalnan17 = Font.custom("Jalnan2", size: 17)
+        static let jalnan16 = Font.custom("Jalnan2", size: 16)
+        static let jalnan15 = Font.custom("Jalnan2", size: 15)
+        static let jalnan14 = Font.custom("Jalnan2", size: 14)
+        static let jalnan13 = Font.custom("Jalnan2", size: 13)
+        static let jalnan12 = Font.custom("Jalnan2", size: 12)
+        static let jalnan11 = Font.custom("Jalnan2", size: 11)
+        static let jalnan10 = Font.custom("Jalnan2", size: 10)
+        
+        static let tmoneyRoundWindRegular15 = Font.custom("TmoneyRoundWind-Regular", size: 15)
+        
+        static let tmoneyRoundWindExtraBold15 = Font.custom("TmoneyRoundWind-ExtraBold", size: 15)
     }
     
 }
---
+++ ToDoMeong/NavigationLazyView.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationLazyView.swift
+//  ToDoMeong
+//
+//  Created by 권대윤 on 9/13/24.
+//
+
+import Foundation
