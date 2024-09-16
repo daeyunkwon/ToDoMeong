fb55a528a615293ab236365979707265c86734f5
Daeyun <sweet_ray@naver.com>
2024-09-16 17:34:00 +0900

    Feat: onChange 분기 처리 Wrapper 객체 생성

---
+++ ToDoMeong/OnChangeWrapper.swift
@@ -0,0 +1,36 @@
+//
+//  OnChangeWrapper.swift
+//  ToDoMeong
+//
+//  Created by 권대윤 on 9/16/24.
+//
+
+import SwiftUI
+
+struct OnChangeWrapper<T: Equatable>: ViewModifier {
+    
+    let value: T
+    
+    let action: (T) -> Void
+    
+    func body(content: Content) -> some View {
+        
+        if #available(iOS 17.0, *) {
+            content
+                .onChange(of: value) {
+                    action(value)
+                }
+        } else {
+            content
+                .onChange(of: value, perform: { value in
+                    action(value)
+                })
+        }
+    }
+}
+
+extension View {
+    func onChange<T: Equatable>(value: T, action: @escaping (T) -> Void) -> some View {
+        modifier(OnChangeWrapper(value: value, action: action))
+    }
+}
