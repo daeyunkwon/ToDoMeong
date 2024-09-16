9d964c5ea1eabb01e59762cfb75b6bc2986666f6
Daeyun <sweet_ray@naver.com>
2024-09-16 15:26:13 +0900

    Feat: Todo에 대한 Realm 데이터 모델 설계

---
+++ ToDoMeong/Model/Todo.swift
@@ -0,0 +1,24 @@
+//
+//  Todo.swift
+//  ToDoMeong
+//
+//  Created by 권대윤 on 9/16/24.
+//
+
+import SwiftUI
+import RealmSwift
+
+final class Todo: Object, ObjectKeyIdentifiable {
+    
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var content: String
+    @Persisted var done: Bool
+    @Persisted var createDate: Date
+    @Persisted var photo: String?
+    
+    convenience init(content: String) {
+        self.init()
+        self.content = content
+        self.done = false
+    }
+}
