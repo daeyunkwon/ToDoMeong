c560224a6e83bbf7dc531cb7c3ec2466f7a9d7bd
Daeyun <sweet_ray@naver.com>
2024-09-18 16:39:31 +0900

    Feat: 투두 수정 화면 UI 구현

---
+++ ToDoMeong/Presentation/Todo/Edit/EditTodoView.swift
@@ -0,0 +1,127 @@
+//
+//  EditTodoView.swift
+//  ToDoMeong
+//
+//  Created by 권대윤 on 9/17/24.
+//
+
+import SwiftUI
+
+struct EditTodoView: View {
+    
+    @StateObject var viewModel: EditTodoViewModel
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .trailing) {
+                self.textFieldView
+            }
+            .padding(.bottom, 5)
+            
+            photoButtonView
+            
+            HStack {
+                self.saveButtonView
+                self.deleteButtonView
+            }
+        }
+        .frame(height: 230)
+        .background {
+            Rectangle()
+                .fill(Color(uiColor: .systemBackground))
+                .clipShape(.rect(topLeadingRadius: 40, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: 40, style: .continuous))
+            Rectangle()
+                .fill(Color(uiColor: .systemBackground))
+                .clipShape(.rect(topLeadingRadius: 0, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: 0, style: .continuous))
+                .offset(y: 50)
+        }
+    }
+    
+    @ViewBuilder private var textFieldView: some View {
+        TextField("내용을 입력해 주세요", text: Binding(get: {
+            viewModel.output.text
+        }, set: { newValue in
+            viewModel.input.text.send(newValue)
+        }))
+        .font(.system(size: 14))
+        .padding()
+        .frame(maxWidth: .infinity)
+        .frame(height: 50)
+        .background(Color(uiColor: .systemGray6))
+        .clipShape(.rect(cornerRadius: 15))
+        .overlay(
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color.clear, lineWidth: 2)
+        )
+        .padding(.horizontal, 15)
+        
+        if !viewModel.output.text.isEmpty {
+            Button(action: {
+                viewModel.input.text.send("")
+            }) {
+                Image(systemName: "xmark.circle.fill")
+                    .foregroundColor(.gray)
+                    .padding(.trailing, 20)
+            }
+        }
+    }
+    
+    private var photoButtonView: some View {
+        Button(action: {
+            
+        }, label: {
+            Image("add_image", bundle: nil)
+                .renderingMode(.template)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 22, height: 22)
+                .tint(.skyblue)
+            Text("사진 추가하기")
+                .tint(Color(uiColor: .label))
+                .font(.system(size: 14))
+            Spacer()
+        })
+        .padding(.leading, 25)
+        .padding(.bottom, 15)
+    }
+    
+    private var saveButtonView: some View {
+        Button(action: {
+            //viewModel.input.addButtonTapped.send(())
+        }, label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(height: 50)
+                    .foregroundStyle(.skyblue)
+                Text("저장")
+                    .font(Constant.AppFont.tmoneyRoundWindExtraBold15)
+                    .foregroundStyle(.white)
+            }
+        })
+        .padding(.leading, 15)
+        .buttonStyle(PlainButtonStyle())
+        .disabled(viewModel.output.text.trimmingCharacters(in: .whitespaces).isEmpty ? true : false)
+    }
+    
+    private var deleteButtonView: some View {
+        Button(action: {
+            //viewModel.input.addButtonTapped.send(())
+            
+        }, label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(height: 50)
+                    .foregroundStyle(.customPink)
+                Text("삭제")
+                    .font(Constant.AppFont.tmoneyRoundWindExtraBold15)
+                    .foregroundStyle(.white)
+            }
+        })
+        .padding(.trailing, 15)
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
+#Preview {
+    EditTodoView(viewModel: EditTodoViewModel(todoItem: Todo(content: "dummy"), isPresented: .constant(true)))
+}
---
+++ ToDoMeong/Presentation/Todo/Edit/EditTodoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditTodoViewModel.swift
+//  ToDoMeong
+//
+//  Created by 권대윤 on 9/17/24.
+//
+
+import Foundation
