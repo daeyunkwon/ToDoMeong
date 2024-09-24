79996f84ebd6bdd5f4ed045f69144c7829edab3e
Daeyun <sweet_ray@naver.com>
2024-09-24 22:09:51 +0900

    Refactor: Date.FormatStyle을 이용해 다국어 대응 날짜로 표시될 수 있도록 변경

--- ToDoMeong/Date+Extension.swift
+++ ToDoMeong/Date+Extension.swift
@@ -9,61 +9,13 @@ import Foundation
 
 extension Date {
     
-    var localDate: Date {
-        let timeZone: TimeZone
-        let currentLocale = Locale.current
-        
-        if currentLocale.region == "KR" {
-            timeZone = TimeZone(identifier: "Asia/Seoul") ?? TimeZone.current
-        } else if currentLocale.region == "JP" {
-            timeZone = TimeZone(identifier: "Asia/Tokyo") ?? TimeZone.current
-        } else {
-            timeZone = TimeZone(identifier: "America/New_York") ?? TimeZone.current
-        }
-        
-        let secondsFromGMT = timeZone.secondsFromGMT(for: self)
-        return self.addingTimeInterval(TimeInterval(secondsFromGMT))
-    }
-    
     var dayOfTheWeekDateString: String {
-        let formatter = DateFormatter()
-        let currentLocale = Locale.current
-        let timeZone: TimeZone
-        
-        if currentLocale.region == "KR" {
-            timeZone = TimeZone(identifier: "Asia/Seoul") ?? TimeZone.current
-            formatter.locale = Locale(identifier: "ko_KR")
-            formatter.dateFormat = "M월 d일 EEEE"
-        } else if currentLocale.region == "JP" {
-            timeZone = TimeZone(identifier: "Asia/Tokyo") ?? TimeZone.current
-            formatter.locale = Locale(identifier: "ja_JP")
-            formatter.dateFormat = "M月d日 EEEE"
-        } else {
-            timeZone = TimeZone(identifier: "America/New_York") ?? TimeZone.current
-            formatter.locale = Locale(identifier: "en_US")
-            formatter.dateFormat = "EEEE, MMM d"
-        }
-        
-        formatter.timeZone = timeZone
-        
-        return formatter.string(from: self)
+        let formatStyle = Date.FormatStyle.dateTime.month().day().weekday(.wide)
+        return self.formatted(formatStyle)
     }
     
     var yearMonthDateString: String {
-        let formatter = DateFormatter()
-        let currentLocale = Locale.current
-        
-        if currentLocale.region == "KR" {
-            formatter.locale = Locale(identifier: "ko_KR")
-            formatter.dateFormat = "yyyy년 M월"
-        } else if currentLocale.region == "JP" {
-            formatter.locale = Locale(identifier: "ja_JP")
-            formatter.dateFormat = "yyyy年 M月"
-        } else {
-            formatter.locale = Locale(identifier: "en_US")
-            formatter.dateFormat = "yyyy MMM"
-        }
-        
-        return formatter.string(from: self)
+        let formatStyle = Date.FormatStyle.dateTime.year().month()
+        return self.formatted(formatStyle)
     }
 }
